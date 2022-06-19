76e66d9059501c319e944d66036afe4c55941dc1
AldairAzevedo <aldairazevedo@hotmail.com.br>
2022-06-19 19:23:01 -0400

    initial commit

---
+++ cinema.js
@@ -0,0 +1,47 @@
+const catalago = require('./database/catalago.json');
+const fs = require('fs');
+const diretorio = '/database/catalago.json';
+
+let json = JSON.stringify(catalago);
+let filmesArray = JSON.parse(json);
+
+const listarTodosOsFilmes = () => {
+  filmesArray.forEach((filme) => {
+    for (let prop in filme) {
+      console.log(`${prop}: ${filme[prop]}`);
+    }
+  });
+}
+
+const listarFilmesEmCartaz = () => {
+  filmesArray.forEach((filme) => {
+    if (filme.cartaz === 'Sim') {
+      for (let prop in filme) {
+        console.log(`${prop}: ${filme[prop]}`);
+      }
+    }
+  });
+}
+
+function save(content) {
+  const contentString = JSON.stringify(content);
+  return fs.writeFile(diretorio, contentString, { enconding: 'utf-8', flag: 'a' }, function (err) {
+    if (err) throw err;
+    console.log('Arquivo salvo!');
+  });
+};
+
+
+const alterarStatusEmCartaz = (index) => {
+  if (filmesArray[index].cartaz === 'Sim') {
+    filmesArray[index].cartaz = 'Não';
+  } else {
+    filmesArray[index].cartaz = 'Sim';
+  }
+  return filmesArray;
+}
+alterarStatusEmCartaz(2);
+//listarFilmesEmCartaz();
+//listarTodosOsFilmes();
+
+save(filmesArray);
